0f2b895bfeae7c0b79c8e863b917d975db906c43
Ayush Kumar Shukla <spositive123@gmail.com>
2022-10-12 16:27:09 +0530

    completed triangle star pattern assignments

---
+++ shellscripts/lstarpat.bash
@@ -0,0 +1,29 @@
+	pattern()
+{
+	for ((i=0;i<$1;i++))
+	do
+		for ((j=0;j<$1;j++))
+		do
+			if [[ $j -lt $(($1-$i-1)) ]]
+			then
+				echo -n " "
+			else
+				echo -n "*"
+			fi
+		done
+		echo
+	done
+		
+
+}
+
+
+echo -n "Enter the number of lines needed in the pattern: "
+read usrnum
+if [[ $usrnum -le 0 ]]
+then
+	echo "There must be atleast one line"
+else
+	pattern $usrnum
+fi
+
