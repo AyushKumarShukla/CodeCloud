d04354ccb416351e7aaf3eb94b7ee48fb1588cd3
AyushKumarShukla <spositive123@gmail.com>
2022-11-11 14:10:14 +0530

    Mostly Done...1st solution needs to be fixed

--- numerical_methods/Gauss_Elimination.c
+++ numerical_methods/Gauss_Elimination.c
@@ -3,18 +3,18 @@
 #include<math.h>
 #include<string.h>
 
-void getmatrix(float (*mat)[20],int row,int col)
+void getmatrix(double (*mat)[20],int row,int col)
 {
     int i,j;
     for(i=0;i<row;i++)
         for(j=0;j<col;j++)
-            scanf("%f",&mat[i][j]);
+            scanf("%lf",&mat[i][j]);
     printf("Enter the values of the right hand vector: \n");
     for(i=0;i<col;i++)
-            scanf("%f",&mat[i][col]);
+            scanf("%lf",&mat[i][col]);
 }
 
-void dispmatrix(float (*mat)[20],int row,int col)
+void dispmatrix(double (*mat)[20],int row,int col)
 {
     int i,j;
     for(i=0;i<row;i++)
@@ -25,7 +25,7 @@ void dispmatrix(float (*mat)[20],int row,int col)
     }
 }
 
-void row_oper(float(*mat)[20],int rc,int r,int ord,float mult)
+void row_oper(double(*mat)[20],int rc,int r,int ord,double mult)
 {
     int j;
     for(j=0;j<ord+1;j++)
@@ -34,43 +34,45 @@ void row_oper(float(*mat)[20],int rc,int r,int ord,float mult)
     }
 }
 
-void Gauss_Elimination(float(*mat)[20],int ord)
+void back_sub(double(*mat)[20],int ord)
+{
+	int i,j;
+	double x[20],sum=0;
+	x[ord-1]=mat[ord-1][ord]/mat[ord-1][ord-1];
+	for(i=ord-2;i>=0;i--)
+	{
+		sum=0;
+		for(j=ord-1;j>=i;j--)
+			sum=sum+(mat[i][j]*x[j]);
+		x[i]=(mat[i][ord]-sum)/mat[i][i];
+	}
+	printf("The Solutions are: \n");
+	for(i=0;i<ord;i++)
+		printf("x%d=%lf\n",i,x[i]);
+}
+
+void Gauss_Elimination(double(*mat)[20],int ord)
 {
     int i,j;
-    float mult,soln=0.0f;
+    double mult,soln=0.0f;
     for(i=1;i<ord;i++)
         for(j=0;j<i;j++)
         {
             mult=-(mat[i][j]/mat[j][j]);
             row_oper(mat,i,j,ord,mult);
         }
-    printf("Coefficient Matrix After Operations:\n");
+    printf("Augmented Matrix After Operations:\n");
     dispmatrix(mat,ord,ord+1);
-    for(i=ord-1;i>=0;i--)
-    {
-        for(j=ord-1;j>=i;j--)
-        {
-            if(mat[i][j-1]==0.0f)
-            {
-                soln=(mat[i][ord]-soln)/mat[i][j];
-                printf("\nsolnfinal:%f",soln);
-            }
-            else
-            {
-                soln=soln*mat[i][j];
-                printf("\nsoln:%f",soln);
-            }
-        }
-        
-    }
+    back_sub(mat,ord);
 }
 
 int main(void)
 {
-    float mat[20][20];
+    double mat[20][20];
     int ord;
     memset(mat,0.0f,sizeof(mat));
-    printf("Enter the order of the coefficient matrix: ");
+    printf("To solve a system of linear equations using Gauss Elimination Method");
+    printf("\nEnter the order of the coefficient matrix: ");
     scanf("%d",&ord);
     printf("Enter %d elements of the coefficient matrix: \n",ord*ord);
     getmatrix(mat,ord,ord);
