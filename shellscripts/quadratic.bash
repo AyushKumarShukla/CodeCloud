13f00085628257ae91285cc8a57d6065f2b5d075
Ayush Kumar Shukla <spositive123@gmail.com>
2022-11-27 14:16:53 +0530

    Completed QuadEqns

--- shellscripts/quadratic.bash
+++ shellscripts/quadratic.bash
@@ -1,25 +1,26 @@
 quadratic()
 {
-	disc=`echo "$B^2-4*$A*$C" | bc -l &2> /dev/null`
+	disc=`echo "$2^2-4*$1*$3" | bc -l`
 	#echo -n "DISC:$disc"
-	if [[ $disc < 0 ]];
+	if [[ "$disc" -lt 0 ]]
 	then
 		echo "Roots are Imaginary"
-	elif [[ $disc = 0 ]];
+		exit 0 
+	elif [[ "$disc" -eq 0 ]]
 	then
 		echo "Roots are equal"
 	else
 		echo "Roots are distinct"
 	fi 
-	res1=`echo "(-($B)+sqrt($disc))/(2*$A)" | bc -l`
-	res2=`echo "(-($B)-sqrt($disc))/(2*$A)" | bc -l`
+	res1=`echo "scale=3;(-($B)+sqrt($disc))/(2*$A)" | bc -l`
+	res2=`echo "scale=3;(-($B)-sqrt($disc))/(2*$A)" | bc -l`
 	echo "Roots are $res1 and $res2"
 }
 
 echo "To solve a quadratic equation of the form Ax^2 + Bx + C = 0"
-read -p 'Enter A' A
-read -p 'Enter B' B
-read -p 'Enter C' C
+read -p 'Enter A: ' A
+read -p 'Enter B: ' B
+read -p 'Enter C: ' C
 quadratic $A $B $C 
 
  
