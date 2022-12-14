1cb0ba6574d18be8652ad1de9b2e59bebe6e2a7a
Ayush Kumar Shukla <spositive123@gmail.com>
2022-12-14 19:52:19 +0530

    Altered leapyear for assignments

--- shellscripts/leapyear.sh
+++ shellscripts/leapyear.sh
@@ -1,9 +1,22 @@
-echo "Enter a year"
-read year
 
-if [ `expr $year % 4` -eq 0 -a `expr $year % 100` -ne 0 -o `expr $year % 400` -eq 0 ]
+is_leapyear()
+{
+	if [ `expr $1 % 4` -eq 0 ] &&  [ `expr $1 % 100` -ne 0 ] || [ `expr $1 % 400` -eq 0 ]
+	then
+		echo "$year is a leap year"
+	else
+		echo "$year is NOT a leap year"
+	fi
+}
+
+echo "To check whether a year is a leapyear "
+read -p 'Enter a year: ' year
+
+#input validation on a pre-assumed range of years
+if [ $year -lt 1000 ] || [ $year -gt 9999 ]
 then
-	echo "LEAP YEAR"
-else
-	echo "NOT A LEAP YEAR"
+	echo "Invalid year"
+	exit 1
 fi
+is_leapyear $year
+
