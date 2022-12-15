da3759246dccfae3bb7d22d42eefc1a044b8f945
Ayush Kumar Shukla <spositive123@gmail.com>
2022-12-15 20:33:55 +0530

    optimized switchcasecalc

--- shellscripts/switchcase_cal.sh
+++ shellscripts/switchcase_cal.sh
@@ -1,29 +1,35 @@
-add ()
+add()
 {
-	echo  "$1+$2" | bc -l
+	echo  "$1 + $2" | bc -l
 }
-sub ()
+
+sub()
 {
 	echo  "$1 - $2" | bc -l
 }
-mult ()
+
+mult()
 {
 	echo  "$1 * $2" | bc -l
 }
-div ()
+
+div()
 {
-	if [ echo "$1 == $2" | bc -l ]
-	then
-		echo "DIVIDE BY ZERO ERROR"
-	else
-		echo "$1/$2" | bc -l
-	fi
+	echo "scale=5;$1 / $2" | bc -l
+}
+
+expo()
+{
+	echo "scale=5;$1 ^ $2" | bc -l
+}
+
+get_input()
+{
+	read -p 'Enter the first number: ' num1
+	read -p 'Enter the second number: ' num2
 }
 
 echo "To perform Arithmetic operations on two numbers: "
-read -p 'Enter the first number: ' num1
-read -p 'Enter the second number: ' num2
-
 while :
 do
 cat << mydoc
@@ -32,29 +38,52 @@ MENU:
 2.SUBTRACTION
 3.MULTIPLICATION
 4.DIVISION
-5.EXIT
+5.EXPONENTIATION
+6.EXIT
 mydoc
 	read -p 'Enter your choice: ' ch
 	case "$ch" in
 	"1")
+	get_input
 	echo -n "SUM: "
 	add $num1 $num2
 	;;
+
 	"2")
+	get_input
 	echo -n "DIFFERENCE: "
 	sub $num1 $num2
 	;;
+
 	"3")
+	get_input
 	echo -n "PRODUCT:"
 	mult $num1 $num2
 	;;
+
 	"4")
+	read -p 'Enter divisor: ' num2
+	if [ `echo "$num2 == 0" | bc -l` -eq 1 ]
+	then
+		echo "DIVIDE BY ZERO ERROR"
+		exit 1
+	fi
+	read -p 'Enter dividend ' num1
 	echo -n "QUOTIENT: "
 	div $num1 $num2
 	;;
+
 	"5")
+	read -p 'Enter the base: ' num1
+	read -p 'Enter the exponent: ' num2
+	echo -n "RESULT: "
+	expo $num1 $num2
+	;;
+
+	"6")
 	break
 	;;
+
 	*)
 	echo -n "Invalid Choice......."
 	;;
