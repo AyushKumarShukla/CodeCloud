6f6f2c293368947aa4c359058bf43d9b26925d5f
Ayush Kumar Shukla <spositive123@gmail.com>
2022-12-25 16:59:35 +0530

    Update #2

--- shellscripts/database.bash
+++ shellscripts/database.bash
@@ -1,21 +1,33 @@
+#!/usr/bin/bash
 insert_rec()
 {
 	today=`date`
-	echo -e "Entry Time: $today\nID\t\tNAME\t\tDESIGNATION\t\tSALARY(In LPA)" >> datafile.txt
+	echo  "Entry Time: $today " >> $1
+	printf "%-10s %-20s %-20s %-10s" "ID" "NAME" "DESIGNATION" "SALARY(In LPA)" >> $1
 	read -p 'Enter Employee ID: ' eid
 	read -p 'Enter Employee NAME: ' ename
 	read -p 'Enter Employee DESIGNATION: ' edes
 	read -p 'Enter Employee SALARY In Lakhs Per Annum: ' esal
-	echo -e "$eid\t$ename\t$edes\t\t$esal\n" >> datafile.txt
+	echo >> $1
+	printf "%-10s %-20s %-20s %-10s\n" "$eid" "$ename" "$edes" "$esal" >> $1
 }
 
 view_rec()
 {
-	read -p 'Enter the employee ID: ' eid
-	grep "$eid" "datafile.txt"
+	read -p 'Enter the Employee ID: ' eid
+	printf "%-10s %-20s %-20s %-10s\n" "ID" "NAME" "DESIGNATION" "SALARY(In LPA)"
+	grep "$eid" "$1"
+	if [ $? -ne 0 ]
+	then
+		echo "No entry with Employee ID $eid"
+	fi
 }
 
-echo "Welcome to the employee database:" 
+echo "Welcome to the employee database:"
+read -p 'Enter a filename to hold the database: ' database
+dir=`pwd`
+if test -e "$database"
+then
 while :
 do
 cat << EOF
@@ -24,21 +36,24 @@ MENU:
 2. VIEW EMPLOYEE RECORD
 3. EXIT
 EOF
-	read -p 'Enter your choice: ' ch
-	if [ $ch -eq 3 ]
-	then
-		exit 0
-	fi
-	case $ch in
-	1)
-		insert_rec
-		;;
-	2)
-		view_rec
-		;;
-	*)
-		echo "Invalid Choice"
-		;;
-	esac
+		read -p 'Enter your choice: ' ch
+		if [ $ch -eq 3 ]
+		then
+			echo "The entries have been stored in the file $database in path $dir"
+			exit 0
+		fi
+		case $ch in
+		1)
+			insert_rec $database
+			;;
+		2)
+			view_rec $database
+			;;
+		*)
+			echo "Invalid Choice"
+			;;
+		esac
 done
-
+else
+	echo "The file $database does not exist in the current path $dir"
+fi
