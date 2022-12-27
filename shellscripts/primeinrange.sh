e1b517d080f940310648bc1827bd798f71aae262
Ayush Kumar Shukla <spositive123@gmail.com>
2022-12-27 17:49:09 +0530

    Learn to group conditions in if statement

--- shellscripts/primeinrange.sh
+++ shellscripts/primeinrange.sh
@@ -1,8 +1,14 @@
 echo "To print all the prime numbers in a given range "
 read -p 'Enter the lower limit: ' low
 read -p 'Enter the upper limit: ' up
-is_prime (){
 
+if [ $up -lt $low ]
+then
+	echo "Invalid Input"
+	exit 1
+fi
+
+is_prime (){
 	i=2
 	flag=1
 	while [ $i -lt $1 ]
@@ -10,15 +16,12 @@ is_prime (){
 		if [ $(( $1 % $i )) -eq 0 ]
 		then
 			flag=0
-		        
 		fi
 		i=$(($i+1))
 	done
-
 }
 
 prime_in_range (){
-
 	i=$1
 	while [ $i -le $2 ]
 	do
