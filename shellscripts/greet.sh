3cf4d3179c0b1a79c613f6d6e1b1be328b81fe96
Ayush Kumar Shukla <spositive123@gmail.com>
2022-12-30 18:51:11 +0530

    Added more greetings

--- shellscripts/greet.sh
+++ shellscripts/greet.sh
@@ -1,10 +1,13 @@
 var=`date +"%H"`
-if [ $var -ge 0 -a $var -lt 12 ]
+if [ $var -ge 4 -a $var -lt 12 ]
 then
 	echo "GOOD MORNING"
 elif [ $var -ge 12 -a $var -le 16 ]
 then
 	echo "GOOD AFTERNOON"
-else
+elif [ $var -ge 17 -a $var -le 22 ]
+then
 	echo "GOOD EVENING"
+else
+	echo "GOOD NIGHT"
 fi
