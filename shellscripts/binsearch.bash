ce2de1919b085b228817fb5c9b6adb9cb6e8848e
Ayush Kumar Shukla <spositive123@gmail.com>
2023-01-06 20:28:55 +0530

    Optimised BinSearch

--- shellscripts/binsearch.bash
+++ shellscripts/binsearch.bash
@@ -1,11 +1,3 @@
-get_array()
-{
-	for((i=0;i<$1;i++))
-	{
-		read temp;
-		arr[$i]=$temp
-	}
-}
 bin_search()
 {
 	beg=0
@@ -13,37 +5,28 @@ bin_search()
 	while [[ $beg -le $end ]]
 	do
 		mid=$(( ($beg+$end)/2 ))
-		mid=${mid/.*}
-		if((`echo "${arr[$mid]} == $2 " | bc -l`))
+		if [[ `echo "${arr[$mid]} == $2 " | bc -l` -eq 1 ]]
 		then
 			flag=1
 			pos=$mid
 			break
-		elif((`echo "${arr[$mid]} <  $2 " | bc -l`))
+		elif [[ `echo "${arr[$mid]} <  $2 " | bc -l` -eq 1 ]]
 		then
-			end=$(($mid-1))
+			beg=$((mid+1))
 		else
-			beg=$(($mid+1))
+			end=$((mid-1))
 		fi
 	done
 }
-
 echo "To perform binary search on a list of numbers"
-read -p 'Enter the number of elements needed in the list: ' usrnum
-if (($usrnum < 1 ))
-then
-	echo "Invalid list size , the size of the list must be atleast 1"
-	exit 1 
-fi
-
-echo "Enter $usrnum Elements in decending order : "
+echo "Enter array elements in ascending order: "
 flag=0
-get_array $usrnum
-
+read -a arr
+arr=(`printf "%s\n" "${arr[@]}" | sort -n`)
+usrnum=${#arr[@]}
 echo "Entered Elements: ${arr[*]}"
 read -p 'Enter the number to be searched: ' key
 bin_search $usrnum $key
-
 if (($flag == 1))
 then
 	echo "Element $key found at position $(($pos+1))"
