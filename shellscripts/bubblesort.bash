7fdd90af4fab66564505c21231932a295ef24bbd
Ayush Kumar Shukla <spositive123@gmail.com>
2023-01-07 20:05:17 +0530

    Optimised bub sort

--- shellscripts/bubblesort.bash
+++ shellscripts/bubblesort.bash
@@ -1,32 +1,30 @@
-
-get_array()
-{
-	for((i=0;i<$1;i++))
-	do
-		read val;
-		arr[$i]=$val
-	done
-}
-
 bubble_sort()
 {
+	k=0
 	for((i=0;i<$1;i++))
 	do
-		for((j=0;j<$1;j++))
+		for((j=0;j<$(($1-1-i));j++))
 		do
 			if [[ ${arr[$j]} -gt ${arr[$j+1]} ]]
 			then
-				temp=${arr[$j]};
-				arr[$j]=${arr[$j+1]};
-				arr[$j+1]=$temp;
+				temp="${arr[$j]}";
+				arr[$j]="${arr[$j+1]}";
+				arr[$j+1]="$temp";
 			fi
+			echo -e "Pass $((k+1)): ${arr[*]}"
+			k=$((k+1))
 		done
-		echo "Pass $((i+1)): ${arr[*]}"
 	done
 }
 echo "To sort an array using bubble sort:  "
-read -p 'Enter the number of elements needed: ' size
-get_array $size
+echo -n "Enter the elements of the list: "
+read -a arr
+size=${#arr[@]}
+if [ $size -le 0 ]
+then
+	echo "Invalid input, there must be atleast one entry"
+	exit 1
+fi
 echo "Entered Array: ${arr[*]}"
 bubble_sort $size
 echo "AFTER SORTING: ${arr[*]}"
