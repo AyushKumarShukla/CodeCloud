83f943dc12bf0abe523132d55f23cb1556570ce2
Ayush Kumar Shukla <spositive123@gmail.com>
2023-01-12 18:01:18 +0530

    Finalised trapezoidal mathod

--- numerical_methods/trapezoidal.c
+++ numerical_methods/trapezoidal.c
@@ -5,24 +5,29 @@
 float expression(float x)
 {
 	float val;
-	val = x*x*x;
+	//val = 1/pow((1+x),2);
+	val=1/(1+2*x);
 	return val;
 }
 
 float trapezoidal(float low,float high,float divnum)
 {
 	float steps,first,last,i,sum=0,arg,result;
+	int cnter=0;
 	steps = ((high-low)/divnum);
 	first=expression(low);
 	last=expression(high);
-	printf("Values Of Other Terms:\n");
+	printf("Table of values of function for each step:\n");
+	printf("No.\t\tx\t\t  y=f(x)\n");
+	printf("%-10d %-20f %-20f\n",cnter,low,first);
 	for(i=1;i<=(divnum-1);i++)
 	{
+		cnter++;
 		arg=low+(steps*i);
 		sum=sum+expression(arg);
-		printf("x\t\ty\n");
-		printf("%f\t%f\n",arg,sum);
+		printf("%-10d %-20f %-20f\n",cnter,arg,sum);
 	}
+	printf("%-10d %-20f %-20f\n",cnter+1,high,last);
 	result=(steps/2)*(first+last+2*sum);
 	return result;
 }
@@ -30,7 +35,8 @@ float trapezoidal(float low,float high,float divnum)
 int main(void)
 {
 	float low,high,divnum,res;
-	printf("To calculate the integral of x^3\n");
+	//printf("To calculate the integral of 1/(1+x)^2\n");
+	printf("To calculate the integral of 1/(1+2xd)\n");
 	printf("Enter the lower limit: ");
 	scanf("%f",&low);
 	printf("Enter the higher limit: ");
@@ -39,6 +45,8 @@ int main(void)
 	scanf("%f",&divnum);
 	res=trapezoidal(low,high,divnum);
 	printf("INTEGRAL:%f",res);
+	printf("\n");
+	return 0;
 }
 
 
