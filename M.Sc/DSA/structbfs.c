c8b1bd4c56f57e8440a164949ed37a72a7a47152
Ayush Kumar Shukla <spositive123@gmail.com>
2024-12-04 22:48:34 +0530

    BFS done

--- M.Sc/DSA/structbfs.c
+++ M.Sc/DSA/structbfs.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
+#include<stdbool.h>
 #define MAX 20
 
 typedef struct{
-	double values[MAX];
+	int values[MAX];
 	int rear;
 	int front;
 }Queue;
@@ -12,6 +13,78 @@ typedef struct{
 	int vcount;
 	int ecount;
 }Graph;
+
+void initQueue(Queue* queue)
+{
+    queue->front = -1;
+    queue->rear = -1;
+}
+
+void Enqueue(Queue* queue,int arg)
+{
+    if(queue->rear >= MAX - 1)
+        printf("Queue Full");
+    else
+    {
+        if(queue->front == -1)
+            queue->front = 0;
+
+        queue->rear = queue->rear + 1;
+        queue->values[queue->rear] = arg;
+    }
+
+}
+
+int Dequeue(Queue* queue)
+{
+    int ret;
+    if(queue->front == -1 || queue->front > queue->rear)
+    {
+        printf("Queue empty");
+        return -1;
+    }
+    else
+    {
+        ret = queue->values[queue->front];
+        queue->front = queue->front + 1;
+        if(queue->front > queue->rear)
+            initQueue(queue);
+        return ret;
+    }
+}
+
+void bfs(Graph* graph)
+{
+    int i,j,ret;
+    bool visited[MAX] = {false};
+    Queue queue;
+    initQueue(&queue);
+
+    for(i=0;i<graph->vcount;i++)
+    {
+        if(!visited[i])
+        {
+            Enqueue(&queue,i);
+            visited[i] = true;
+
+            while(queue.front != -1)
+            {
+                ret = Dequeue(&queue);
+                printf("%d\t",ret);
+
+                for(j=0;j<graph->vcount;j++)
+                {
+                    if(graph->values[ret][j] == 1 && !visited[j])
+                    {
+                        Enqueue(&queue,j);
+                        visited[j] = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 void initGraph(Graph* graph,int vcount,int ecount)
 {
@@ -60,6 +133,10 @@ int main(void)
 	
 	initGraph(&graph,vcount,ecount);
 	inputGraph(&graph);
+    printf("Generated Adjacency Matrix:\n");
 	showGraph(&graph);
+    printf("\n\nBFS TRAVERSAL:\t");
+    bfs(&graph);
+    printf("\n");
 	return 0;
 }
